4c374c8220f913b965314960a02d19480773c5c8
Carl Fürstenberg <carl@excito.com>
2010-03-27 02:02:18 +0100

    APNG support
    This test utilises canvas to detect if we support apng or will only show
    them as plain png.

--- src/support.js
+++ src/support.js
@@ -56,11 +56,15 @@
 		// (WebKit defaults to false instead of true, IE too, if it's in an optgroup)
 		optSelected: document.createElement("select").appendChild( document.createElement("option") ).selected,
 
+		// do we support canvas
+		canvas: !!document.createElement('canvas').getContext,
+
 		// Will be defined later
 		checkClone: false,
 		scriptEval: false,
 		noCloneEvent: true,
-		boxModel: null
+		boxModel: null,
+		apng: false
 	};
 
 	script.type = "text/javascript";
@@ -129,6 +133,36 @@
 	
 	jQuery.support.submitBubbles = eventSupported("submit");
 	jQuery.support.changeBubbles = eventSupported("change");
+
+	// We can only test APNG support by using canvas
+	// from http://code.eligrey.com/apng-detect/demo.html
+	// Though inline data seems to work fine, dunno what he is babbling about
+	if( jQuery.support.canvas ) {
+		var apng_data = "data:image/png;base64," +
+			"iVBORw0KGgoAAAANSUhE" +
+			"UgAAAAEAAAABCAYAAAAf" +
+			"FcSJAAAACGFjVEwAAAAB" +
+			"AAAAAcMq2TYAAAANSURB" +
+			"VAiZY2BgYPgPAAEEAQB9" +
+			"ssjfAAAAGmZjVEwAAAAA" +
+			"AAAAAQAAAAEAAAAAAAAA" +
+			"AAD6A+gBAbNU+2sAAAAR" +
+			"ZmRBVAAAAAEImWNgYGBg" +
+			"AAAABQAB6MzFdgAAAABJ" +
+			"RU5ErkJggg==";
+		var img = document.createElement('img');
+		var ctx = document.createElement("canvas").getContext("2d");
+		img.src = apng_data;
+		// frame 1 (skipped on apng-supporting browsers): 0,0,0,255
+		// frame 2: 0,0,0,0
+		img.onload = function(){
+			ctx.drawImage(img, 0, 0);
+			jQuery.support.apng = ctx.getImageData(0, 0, 1, 1).data[3] == 0;
+		};
+
+		img = ctx = null;
+	}
+
 
 	// release memory in IE
 	root = script = div = all = a = null;
